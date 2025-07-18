ee023b21f34841396fb1a753975b24ee32fd529a
Martu Peluso <martupeluso@proton.me>
2025-07-17 23:55:29 -0300

    Make the grid resizable based on user input
    The user can now choose the amount of squares
    per side the grid will use.

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++){
+const button = document.querySelector("button");
+
+const GRID_WIDTH = 600;
+
+button.addEventListener("click", () => {
+  let squaresPerSide;
+
+  do {
+    squaresPerSide = +prompt("How many squares per side should the grid have? (Max. 100)");
+  } while (squaresPerSide == "" || squaresPerSide > 100);
+
+  createGrid(squaresPerSide);
+});
+
+function createGrid(squaresPerSide = 16) {
+  container.textContent = "";
+
+  for (let i = 0; i < squaresPerSide ** 2; i++) {
     const div = document.createElement("div");
 
-    div.style.width = `${600/16}px`;
-    div.style.height = `${600/16}px`;
+    div.style.width = `${GRID_WIDTH/squaresPerSide}px`;
+    div.style.height = `${GRID_WIDTH/squaresPerSide}px`;
     div.style.border = "1px solid black";
 
     div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "lightgreen";
-    })
+      div.style.backgroundColor = "lightgreen";
+    });
 
     container.appendChild(div);
+  }
 }
+
+createGrid();
